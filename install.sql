c6e9323be221662667cf4dac0b1303695e4f8594
Kimpe Andy <andykimpe@gmail.com>
2016-01-29 20:43:23 +0100

    Create install.sql

---
+++ install.sql
@@ -0,0 +1,116 @@
+CREATE DATABASE IF NOT EXISTS `FOS-Streaming` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `FOS-Streaming`;
+
+DROP TABLE IF EXISTS `admins`;
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `admins_username_unique` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+INSERT INTO `admins` (`id`, `username`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2016-01-29 19:38:33', '2016-01-29 19:38:33');
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `category_user`;
+CREATE TABLE IF NOT EXISTS `category_user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `settings`;
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ffmpeg_path` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '/usr/local/bin/ffmpeg',
+  `ffprobe_path` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '/usr/local/bin/ffprobe',
+  `webport` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '8000',
+  `webip` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `hlsfolder` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'hl',
+  `less_secure` tinyint(4) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `streams`;
+CREATE TABLE IF NOT EXISTS `streams` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `streamurl` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `streamurl2` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `streamurl3` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `running` tinyint(4) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `trans_id` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `restream` tinyint(4) NOT NULL,
+  `video_codec_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `audio_codec_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `bitstreamfilter` tinyint(4) NOT NULL,
+  `checker` tinyint(4) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `transcodes`;
+CREATE TABLE IF NOT EXISTS `transcodes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `probesize` bigint(20) NOT NULL,
+  `analyzeduration` bigint(20) NOT NULL,
+  `video_codec` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `audio_codec` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `profile` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `preset_values` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `scale` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `aspect_ratio` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `video_bitrate` bigint(20) NOT NULL,
+  `audio_channel` int(11) NOT NULL,
+  `audio_bitrate` bigint(20) NOT NULL,
+  `fps` int(11) NOT NULL,
+  `minrate` int(11) NOT NULL,
+  `maxrate` int(11) NOT NULL,
+  `bufsize` int(11) NOT NULL,
+  `audio_sampling_rate` int(11) NOT NULL,
+  `crf` int(11) NOT NULL,
+  `threads` int(11) NOT NULL,
+  `deinterlance` tinyint(4) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `transcodes_name_unique` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `active` tinyint(4) NOT NULL,
+  `lastconnected_ip` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `exp_date` date NOT NULL,
+  `last_stream` int(11) NOT NULL,
+  `useragent` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_username_unique` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
