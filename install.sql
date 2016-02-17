63b65d6123fa2f70c3ece54d562e4b4580754aed
Kimpe Andy <andykimpe@gmail.com>
2016-02-17 08:33:02 +0100

    Update install.sql

--- install.sql
+++ install.sql
@@ -1,6 +1,26 @@
 CREATE DATABASE IF NOT EXISTS `FOS-Streaming` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `FOS-Streaming`;
 
+DROP TABLE IF EXISTS `activity`;
+CREATE TABLE IF NOT EXISTS `activity` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `stream_id` int(11) NOT NULL,
+  `user_agent` text COLLATE utf8_unicode_ci NOT NULL,
+  `user_ip` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `pid` int(11) NOT NULL,
+  `bandwidth` int(11) NOT NULL DEFAULT '0',
+  `date_start` datetime NOT NULL,
+  `date_end` datetime NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `activity_user_id_index` (`user_id`),
+  KEY `activity_stream_id_index` (`stream_id`),
+  KEY `activity_date_end_index` (`date_end`),
+  KEY `activity_pid_index` (`pid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
 DROP TABLE IF EXISTS `admins`;
 CREATE TABLE IF NOT EXISTS `admins` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -13,7 +33,29 @@ CREATE TABLE IF NOT EXISTS `admins` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 INSERT INTO `admins` (`id`, `username`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2016-01-29 19:38:33', '2016-01-29 19:38:33');
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2016-01-29 19:38:33', '2016-01-29 18:38:33');
+
+DROP TABLE IF EXISTS `blocked_ips`;
+CREATE TABLE IF NOT EXISTS `blocked_ips` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ip` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `blocked_ips_ip_unique` (`ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `blocked_user_agents`;
+CREATE TABLE IF NOT EXISTS `blocked_user_agents` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `blocked_user_agents_name_unique` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
@@ -45,6 +87,8 @@ CREATE TABLE IF NOT EXISTS `settings` (
   `less_secure` tinyint(4) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `user_agent` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'FOS-Streaming',
+  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '1.0.1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
@@ -111,6 +155,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `useragent` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `max_connections` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_username_unique` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
